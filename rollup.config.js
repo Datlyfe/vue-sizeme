a29c944dc21843ef168fcdfcd108e92b94388b84
datlyfe <mrbouaggadmoez@gmail.com>
2020-02-14 02:13:43 +0100

    rollup config

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import babel from 'rollup-plugin-babel'
+import inject from 'rollup-plugin-inject'
+
+const getConfig = polyfill => {
+  const config = {
+    input: 'src/index.js',
+    output: [],
+    plugins: [babel()],
+    external: ['vue', '@vue/composition-api']
+  }
+
+  if (polyfill) {
+    config.output = [
+      {
+        file: 'polyfilled.js',
+        format: 'cjs'
+      }
+    ]
+    config.external.push('resize-observer-polyfill')
+    config.plugins.push(
+      inject({
+        ResizeObserver: 'resize-observer-polyfill'
+      })
+    )
+  } else {
+    config.output = [
+      {
+        file: 'dist/bundle.cjs.js',
+        format: 'cjs'
+      },
+      {
+        file: 'dist/bundle.esm.js',
+        format: 'esm'
+      }
+    ]
+  }
+
+  return config
+}
+
+export default [getConfig(), getConfig(true)]
