d28e67a127793a3ef7eb0d92602a7346eb51398d
datlyfe <mrbouaggadmoez@gmail.com>
2020-02-14 01:10:26 +0100

    init

---
+++ README.md
@@ -0,0 +1,46 @@
+# vue-sizeme
+
+A Vue3 hook that uses Vue's composition api to make components aware of their width and height.
+
+## Installation
+
+At this point in time Vue version 3 is not released yet so you have to use the [Vue composition api plugin](https://github.com/vuejs/composition-api) in order for this to work.
+
+```sh
+yarn add @vue/composition-api vue-sizeme
+```
+
+```javascript
+import Vue from 'vue'
+import VueCompositionApi from '@vue/composition-api'
+
+Vue.use(VueCompositionApi)
+```
+
+Then in your component
+
+```js
+<template>
+<div ref="root"></div>
+</template>
+
+<script>
+import { useWithSize } from 'vue-sizeme'
+import { ref } from '@vue/composition-api'
+export default {
+  setup() {
+    const root = ref(null)
+    const { width, height } = useWithSize({
+      elementRef: root
+    })
+
+    console.log(state)
+    return {
+      root,
+      width,
+      height
+    }
+  }
+}
+</script>
+```
---
+++ src/index.js
@@ -0,0 +1,63 @@
+import {
+  ref,
+  onMounted,
+  reactive,
+  onUnmounted,
+  computed
+} from '@vue/composition-api'
+
+export const useWithSize = ({ elementRef, onResize } = {}) => {
+  const state = reactive({
+    width: undefined,
+    height: undefined
+  })
+  const previous = ref({
+    width: undefined,
+    height: undefined
+  })
+
+  const resizeObserver = new ResizeObserver(entries => {
+    if (!Array.isArray(entries) || !entries.length) {
+      return
+    }
+
+    const entry = entries[0]
+    const newWidth = Math.round(entry.contentRect.width)
+    const newHeight = Math.round(entry.contentRect.height)
+    if (
+      previous.value.width !== newWidth ||
+      previous.value.height !== newHeight
+    ) {
+      const newSize = { width: newWidth, height: newHeight }
+      if (onResize && typeof onResize === 'function') {
+        onResize(newSize)
+      } else {
+        previous.value.width = newWidth
+        previous.value.height = newHeight
+        state.width = newSize.width
+        state.height = newSize.height
+      }
+    }
+  })
+
+  onMounted(() => {
+    if (
+      typeof elementRef !== 'object' ||
+      elementRef === null ||
+      !(elementRef.value instanceof Element)
+    ) {
+      return
+    }
+    resizeObserver.observe(elementRef.value)
+  })
+
+  onUnmounted(() => {
+    resizeObserver.unobserve(elementRef.value)
+  })
+
+  return computed(() => {
+    return state
+  })
+}
+
+export default useWithSize
